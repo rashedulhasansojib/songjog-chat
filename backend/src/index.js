f31f8eb8cd541663af478a683070f0656ac23008
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-12 23:30:16 +0600

    message related  routes created in backend/

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cookqqieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
-import chatRoutes from "./routes/chat.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cookqqieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
