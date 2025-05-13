53b27729e4c614bde09365ce06b61af83fc993b2
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-13 23:56:09 +0600

    Profile update  page added and connected with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cookqqieParser());
 app.use(
   cors({
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -66,4 +66,18 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const response = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: response.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response?.data?.message || "Error updating profile");
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
