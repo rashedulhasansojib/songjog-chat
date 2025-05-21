3aa4b6d2ecd3b3a8065dcc6a09f8d845b0c1cd59
Rashedul Hasan Sojib <109899676+rashedulhasansojib@users.noreply.github.com>
2025-05-21 22:23:25 +0600

    fix: CLIENT_URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
